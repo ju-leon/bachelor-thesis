992e76162488e68d68e3c4f19ef688275ba5b3fd
Leon Jungemeyer <leon.jungemeyer@me.com>
2021-01-11 13:16:57 +0100

    Fix bash

--- code/batch_job_submitter.sh
+++ code/batch_job_submitter.sh
@@ -2,5 +2,5 @@
 
 for ASTEP in 1 3 5 8 10 12 16 20 50
 do
-    python3 soap_parameter_test.py ../data/ ../out/ --augment_steps=$ASTEP
+    python3 soap_training.py ../data/ ../out/ --augment_steps=$ASTEP
 done
