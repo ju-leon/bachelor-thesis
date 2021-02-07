437b416d96f89e99c420118f4f05602912735216
Leon Jungemeyer <leon.jungemeyer@me.com>
2021-02-07 22:27:13 +0100

    Train models for SOAP

--- code/soap_training.py
+++ code/soap_training.py
@@ -25,6 +25,9 @@ from keras.models import Model
 from keras import regularizers
 import argparse
 
+from kerastuner.tuners import Hyperband
+import kerastuner as kt
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from keras.preprocessing.image import ImageDataGenerator
@@ -38,6 +41,11 @@ from sklearn.preprocessing import StandardScaler
 from soap_generation.alignment import align_elements
 from soap_generation.augment import augment_elements
 
+import os.path
+from os import path
+import sys
+
+input_shape = 0
 
 def read_data(data_dir):
     barriers = dict()
@@ -71,13 +79,14 @@ def save_loss(history, location):
     plt.savefig(location)
 
 
-def save_scatter(train_y_real, train_y_pred, test_y_real, test_y_pred, location):
+def save_scatter(train_y_real, train_y_pred, val_y_real, val_y_pred, test_y_real, test_y_pred, location):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(train_y_real, train_y_pred,
                marker="o", c="C1", label="Training")
+    ax.scatter(val_y_real, val_y_pred, marker="o", c="C3", label="Validation")
     ax.scatter(test_y_real, test_y_pred, marker="o",
-               c="C3", label="Validation")
+               c="C2", label="Validation")
     ax.set_aspect('equal')
     ax.set_xlabel("Calculated barrier [kcal/mol]")
     ax.set_ylabel("Predicted barrier [kcal/mol]")
@@ -94,39 +103,39 @@ def step_decay(epoch):
     return lrate
 
 
-def get_model(input_shape):
+def get_model(hp):
+    global input_shape
+    #input_shape = (12, 48, 1)
     inputs = tf.keras.Input(shape=input_shape)
 
-    x = tf.keras.layers.Dense(550, activation="relu", kernel_regularizer=regularizers.l2(0.15))(inputs)
-    x = tf.keras.layers.Dropout(0.6)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
+    x = inputs
+    x = tf.keras.layers.Flatten()(x)
 
-    #x = tf.keras.layers.Dense(450, activation="relu", kernel_regularizer=regularizers.l2(0.08))(x)
-    #x = tf.keras.layers.Dropout(0.6)(x)
-    #x = tf.keras.layers.BatchNormalization()(x)
+    for i in range(hp.Int('hidden_layers', 1, 6, default=3)):
+        size = hp.Int('hidden_size_' + str(i), 10, 700, step=40)
+        reg = hp.Float('hidden_reg_' + str(i), 0,
+                       0.06, step=0.01, default=0.02)
+        dropout = hp.Float('hidden_dropout_' + str(i),
+                           0, 0.5, step=0.1, default=0.2)
 
-    #x = tf.keras.layers.Dense(300, activation="relu", kernel_regularizer=regularizers.l2(0.02))(x)
-    #x = tf.keras.layers.Dropout(0.4)(x)
+        x = tf.keras.layers.Dense(size, activation="relu",
+                                  kernel_regularizer=regularizers.l2(reg))(x)
+        x = tf.keras.layers.Dropout(dropout)(x)
 
-    x = tf.keras.layers.Dense(340, activation="relu", kernel_regularizer=regularizers.l2(0.08))(x)
-    x = tf.keras.layers.Dropout(0.5)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
+        norm = hp.Choice('hidden_batch_norm_' + str(i), values=[True, False])
 
-    x = tf.keras.layers.Dense(220, activation="relu", kernel_regularizer=regularizers.l2(0.02))(x)
-    x = tf.keras.layers.Dropout(0.3)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-
-    x = tf.keras.layers.Dense(110, activation="relu", kernel_regularizer=regularizers.l2(0.02))(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-
-    x = tf.keras.layers.Dense(64, activation="relu", kernel_regularizer=regularizers.l2(0.01))(x)
-    #x = tf.keras.layers.Dropout(0.3)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
+        if norm:
+            x = tf.keras.layers.BatchNormalization()(x)
 
     x = tf.keras.layers.Dense(1, kernel_regularizer='l2')(x)
 
     model = tf.keras.Model(inputs=inputs, outputs=x)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(
+            hp.Float('learning_rate', 1e-6, 1e-4, sampling='log')),
+        loss='mean_squared_error',
+        metrics=[tf.keras.metrics.MeanSquaredError()])
 
     return model
 
@@ -150,9 +159,6 @@ def main():
     parser.add_argument(
         'out_dir', help='Directory for storing generated features')
 
-    parser.add_argument('--test_split', default=0.2,
-                        help='Size of test fraction from training data', type=float)
-
     parser.add_argument('--augment_steps', default=5,
                         help='NUmber of augmentations around Z axis for every sample', type=int)
 
@@ -162,102 +168,154 @@ def main():
     parser.add_argument('--lmax', default=3,
                         help='Size of test fraction from training data', type=int)
 
-    parser.add_argument('--rcut', default=6.0,
-                        help='Size of test fraction from training data', type=float)
-
     args = parser.parse_args()
+
+    ## Check if hyperparam optimization was run for given pair
+    if not path.exists("Hyperband_SNAP_" + str(args.nmax) + ":" + str(args.lmax)):
+        print("Skipping " + str(args.nmax) + ":" + str(args.lmax))
+        sys.exit()
 
     elems, labels = read_data(args.data_dir)
 
-    number_samples = len(elems)
-
     elems = align_elements(elems)
 
+    number_samples = len(elems)
     elems, labels = augment_elements(elems, labels, args.augment_steps)
 
     species = ["H", "C", "N", "O", "F", "P", "S", "Cl", "As", "Br", "I", "Ir"]
-    rcut = args.rcut
     nmax = args.nmax
     lmax = args.lmax
 
-    soap = dscribe.descriptors.SOAP(
-        species=species,
-        periodic=False,
-        rcut=rcut,
-        nmax=nmax,
-        lmax=lmax,
-        rbf="gto"
-    )
+    for rcut in [5, 10, 20, 30]:
+        for test_split in [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.95]:
+            soap = dscribe.descriptors.SOAP(
+                species=species,
+                periodic=False,
+                rcut=rcut,
+                nmax=nmax,
+                lmax=lmax,
+                rbf="gto"
+            )
 
-    # Create soap coefficients
-    atom_index = [[0]] * len(elems)
-    features_soap = soap.create_coeffs(elems, positions=atom_index)
+            file_identifier = "__augment_steps=" + str(args.augment_steps) + "_l=" + str(
+                lmax) + "_n=" + str(nmax) + "_split=" + str(test_split) + "_rcut=" + str(rcut)
 
-    # Scale coefficents
-    soapScaler = StandardScaler()
-    soapScaler.fit(features_soap)
-    features_soap = soapScaler.transform(features_soap)
+            # Create soap coefficients
+            atom_index = [[0]] * len(elems)
+            features_soap = soap.create_coeffs(elems, positions=atom_index)
 
-    # Scale labels
-    labels = np.array(labels)
-    barrierScaler = StandardScaler()
-    barrierScaler.fit(labels.reshape(-1, 1))
-    labels = barrierScaler.transform(labels.reshape(-1, 1))
+            # Scale coefficents
+            soapScaler = StandardScaler()
+            soapScaler.fit(features_soap)
+            features_soap = soapScaler.transform(features_soap)
 
-    # Reshape so all auguemented data for every sample are either in training or in test data
-    features_soap = features_soap.reshape(
-        number_samples, args.augment_steps, -1)
+            # Scale labels
+            labels = np.array(labels)
+            barrierScaler = StandardScaler()
+            barrierScaler.fit(labels.reshape(-1, 1))
+            labels = barrierScaler.transform(labels.reshape(-1, 1))
 
-    labels = labels.reshape(
-        number_samples, args.augment_steps, -1)
+            # Reshape so all auguemented data for every sample are either in training or in test data
+            features_soap = features_soap.reshape(
+                number_samples, args.augment_steps, -1)
 
-    (trainX, testX, trainY, testY) = train_test_split(
-        features_soap, labels, test_size=args.test_split, random_state=32)
+            print(features_soap.shape)
 
-    trainX = trainX.reshape(-1, features_soap.shape[2])
-    testX = testX.reshape(-1, features_soap.shape[2])
-    trainY = trainY.flatten()
-    testY = testY.flatten()
+            labels = labels.reshape(
+                number_samples, args.augment_steps, -1)
 
-    model = get_model((trainX.shape[1],))
+            # Reserve 10% as validation
+            (features_soap, testX, labels, testY) = train_test_split(
+                features_soap, labels, test_size=0.1, random_state=32)
 
-    opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+            # Split the rest of the data
+            (trainX, valX, trainY, valY) = train_test_split(
+                features_soap, labels, test_size=test_split, random_state=32)
 
-    model.compile(loss="mean_squared_error", optimizer=opt)
+            np.save("features_train_" + str(nmax) + ":" + str(lmax) + ".npy", trainX)
+            np.save("labels_train_" + str(nmax) + ":" + str(lmax) + ".npy", trainY)
 
-    # Train the model
-    H = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=1000,
-                  batch_size=1024, verbose=2, callbacks=[tf.keras.callbacks.LearningRateScheduler(step_decay)])
+            np.save("features_val_" + str(nmax) + ":" + str(lmax) + ".npy", valX)
+            np.save("labels_val_" + str(nmax) + ":" + str(lmax) + ".npy", valY)
 
-    # Save loss of current model
-    save_loss(H, args.out_dir + "loss__augment_steps=" + str(args.augment_steps) +
-              "_l=" + str(lmax) + ",n=" + str(nmax) + ".pdf")
+            np.save("features_test_" + str(nmax) + ":" +
+                    str(lmax) + ".npy", testX)
+            np.save("labels_test_" + str(nmax) + ":" + str(lmax) + ".npy", testY)
 
-    # Scale back
-    train_y_pred = barrierScaler.inverse_transform(model.predict(trainX))
-    train_y_real = barrierScaler.inverse_transform(trainY)
+            trainX = trainX.reshape(-1, 12, int(trainX.shape[2] / 12), 1)
+            valX = valX.reshape(-1, 12, int(valX.shape[2] / 12), 1)
+            testX = testX.reshape(-1, 12, int(testX.shape[2] / 12), 1)
+            trainY = trainY.flatten()
+            valY = valY.flatten()
+            testY = testY.flatten()
 
-    test_y_pred = barrierScaler.inverse_transform(model.predict(testX))
-    test_y_real = barrierScaler.inverse_transform(testY)
+            global input_shape
+            input_shape = trainX[0].shape
 
-    save_scatter(train_y_real, train_y_pred, test_y_real, test_y_pred, args.out_dir +
-                 "scatter__" + str(args.augment_steps) +
-                 "_l=" + str(lmax) + ",n=" + str(nmax) + ".pdf")
+            print(input_shape)
 
-    # Save R2, MAE
-    r2, mae = reg_stats(testY, model.predict(testX), barrierScaler)
-    file = open(args.out_dir + "out.csv", "a")
-    file.write(str(args.augment_steps))
-    file.write(",")
-    file.write(str(args.nmax))
-    file.write(",")
-    file.write(str(args.lmax))
-    file.write(",")
-    file.write(str(r2))
-    file.write(",")
-    file.write(str(mae))
-    file.write("\n")
-    file.close()
+            tuner = kt.Hyperband(
+                get_model,
+                objective='val_mean_squared_error',
+                max_epochs=1200,
+                project_name="Hyperband_SNAP_" + str(nmax) + ":" + str(lmax)
+            )
+
+            best_hp = tuner.get_best_hyperparameters()[0]
+
+            model = get_model(best_hp)
+
+            opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+            model.compile(loss="mean_squared_error", optimizer=opt)
+
+            # Train the model
+            H = model.fit(
+                x=trainX,
+                y=trainY,
+                validation_data=(valX, valY),
+                epochs=2000,
+                batch_size=1024,
+                verbose=2,
+                callbacks=[tf.keras.callbacks.LearningRateScheduler(step_decay),
+                        tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=200)]
+            )
+
+            # Save loss of current model
+            save_loss(H, args.out_dir + "loss__" + file_identifier + ".pdf")
+
+            # Scale back
+            train_y_pred = barrierScaler.inverse_transform(model.predict(trainX))
+            train_y_real = barrierScaler.inverse_transform(trainY)
+
+            val_y_pred = barrierScaler.inverse_transform(model.predict(valX))
+            val_y_real = barrierScaler.inverse_transform(valY)
+
+            test_y_pred = barrierScaler.inverse_transform(model.predict(testX))
+            test_y_real = barrierScaler.inverse_transform(testY)
+
+            save_scatter(train_y_real, train_y_pred, val_y_real, val_y_pred,
+                        test_y_real, test_y_pred, args.out_dir + file_identifier + ".pdf")
+
+            # Save R2, MAE
+            r2, mae = reg_stats(testY, model.predict(testX), barrierScaler)
+            file = open(args.out_dir + "out.csv", "a")
+            file.write(str(args.augment_steps))
+            file.write(",")
+            file.write(str(test_split))
+            file.write(",")
+            file.write(str(args.nmax))
+            file.write(",")
+            file.write(str(args.lmax))
+            file.write(",")
+            file.write(str(rcut))
+            file.write(",")
+            file.write(str(r2))
+            file.write(",")
+            file.write(str(mae))
+            file.write("\n")
+            file.close()
+
+            model.save(args.out_dir + "model__" + file_identifier + ".h5")
 
 
 if __name__ == "__main__":
