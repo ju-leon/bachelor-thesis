ebf47c5695f097288bb35174aad94aa321cf81e5
utpqw@student.kit.edu <utpqw@uc2n997.localdomain>
2021-02-12 11:14:46 +0100

    Bug fixes

--- code/snap_training.sh
+++ code/snap_training.sh
@@ -7,7 +7,7 @@ do
 	do
     		for LMAX in 2 3 4 5 6 7 8 9
     		do
-        		python3 soap_training.py ../data/ ../out_$1/ --augment_steps=$1 --nmax=$NMAX --lmax=$LMAX
+        		python3 soap_training.py ../data/ ../out_$1/ --augment_steps=$1 --rcut=$RCUT --nmax=$NMAX --lmax=$LMAX
     		done
 	done
 done
