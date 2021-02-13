46222fb81693e5cf09577f8402290922283fa915
Leon Jungemeyer <leon.jungemeyer@me.com>
2021-02-13 23:56:04 +0100

    Update training params

--- code/snap_training.sh
+++ code/snap_training.sh
@@ -2,8 +2,5 @@
 
 for BATCH in 2048 512 128 32 8 1
 do
-	python3 soap_training.py ../data/ ../out/ --augment_steps= --nmax=9 --lmax=3 --batch_size=$BATCH
-	python3 soap_training.py ../data/ ../out/ --augment_steps= --nmax=9 --lmax=4 --batch_size=$BATCH
-	python3 soap_training.py ../data/ ../out/ --augment_steps= --nmax=7 --lmax=3 --batch_size=$BATCH
-	python3 soap_training.py ../data/ ../out/ --augment_steps= --nmax=8 --lmax=4 --batch_size=$BATCH
+	python3 soap_training.py ../data/ ../out/ --augment_steps= --nmax=$1 --lmax=$2 --batch_size=$BATCH
 done
